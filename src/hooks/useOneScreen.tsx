c522e566d7ecbc19a273ca83621a17b739003df0
toky <tokyfydev@gmail.com>
2022-10-11 18:55:11 +0300

    Add Hooks to make footer clickable

---
+++ src/hooks/useOneScreen.tsx
@@ -0,0 +1,23 @@
+import React, {useEffect, useState} from "react";
+
+export default function useOnScreen(ref :React.RefObject<HTMLDivElement>) {
+
+    const [isIntersecting, setIntersecting] = useState(false)
+
+    const observer = new IntersectionObserver(
+        ([entry]) => setIntersecting(entry.isIntersecting),
+        {
+            threshold : .65,
+        }
+    )
+
+    useEffect(() => {
+       if(ref.current) {
+           observer.observe(ref.current)
+       }
+        // Remove the observer as soon as the component is unmounted
+        return () => { observer.disconnect() }
+    }, [])
+
+    return isIntersecting
+}
