bec08b7a77e4e7e58ffc4f9e014b226ff11150f2
toky <tokyfydev@gmail.com>
2022-11-18 06:21:32 +0300

    Add coming soon Components for unhandled route

---
+++ src/components/ComingSoon/ComingSoon.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import style from "./ComingSoon.module.scss";
+import Row from "../layouts/layout";
+
+const ComingSoon = () => {
+  return (
+    <Row>
+      <div className={style.comingSoon}>
+        <div>
+          <h1>Coming Sooonnn !</h1>
+          <p>Sorry , This section is under active development !!</p>
+        </div>
+      </div>
+    </Row>
+  );
+};
+
+export default ComingSoon;
