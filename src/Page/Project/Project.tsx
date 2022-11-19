eb16c5369137e8bb13805962384ca46243396402
toky <tokyfydev@gmail.com>
2022-11-19 08:18:42 +0300

    Improve project page styling

--- src/Page/Project/Project.tsx
+++ src/Page/Project/Project.tsx
@@ -14,23 +14,25 @@ const Project: FunctionComponent = () => {
           project and Experience.
         </p>
       </div>
-      <ProjectCard
-        title={"Mozika"}
-        link={"mozika.netlify.app"}
-        description={
-          "A landing page to show all my local favorite artist , with a nice parallax effect on Desktop and a drag and drop on mobile."
-        }
-        image={MozikaApp}
-        ambientColor={"rgba(0, 0, 0, 0.4)"}
-      />
-      <ProjectCard
-        title={"TokyBlog"}
-        link={"tokyblog.netlify.app"}
-        description={
-          "My portfolio and blog , so I can share my knowledge with other developer, I has a admin panel so i write article on the browser"
-        }
-        image={TokyblogApp}
-      />
+      <div className={style.projectList}>
+        <ProjectCard
+          title={"Mozika"}
+          link={"mozika.netlify.app"}
+          description={
+            "A landing page to show all my local favorite artist , with a nice parallax effect on Desktop and a drag and drop on mobile."
+          }
+          image={MozikaApp}
+          ambientColor={"rgba(0, 0, 0, 0.4)"}
+        />
+        <ProjectCard
+          title={"TokyBlog"}
+          link={"tokyblog.netlify.app"}
+          description={
+            "My portfolio and blog , so I can share my knowledge with other developer, I has a admin panel so i write article on the browser"
+          }
+          image={TokyblogApp}
+        />
+      </div>
     </Row>
   );
 };
