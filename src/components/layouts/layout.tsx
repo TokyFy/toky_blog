4314e7770494051490e72b3696cb8fc01c58c227
toky <tokyfydev@gmail.com>
2022-11-20 22:09:10 +0300

    Add section in layout

--- src/components/layouts/layout.tsx
+++ src/components/layouts/layout.tsx
@@ -1,12 +1,16 @@
-import style from './layouts.module.scss';
+import style from "./layouts.module.scss";
 import React from "react";
 
-const Row:React.FC<{children : JSX.Element[] | JSX.Element}> = ({children}) => {
-    return (
-        <div className={style.row}>
-            {children}
-        </div>
-    )
-}
+const Row: React.FC<{ children: JSX.Element[] | JSX.Element }> = ({
+  children,
+}) => {
+  return <div className={style.row}>{children}</div>;
+};
 
-export default Row
+const Section: React.FC<{ children: JSX.Element[] | JSX.Element }> = ({
+  children,
+}) => {
+  return <section className={style.section}>{children}</section>;
+};
+
+export { Row, Section };
