23ca84c7bee4b994823095f7954cc47afad13cbb
toky <tokyfydev@gmail.com>
2022-11-20 22:08:25 +0300

    Add coming soon page

--- src/components/ComingSoon/ComingSoon.tsx
+++ src/components/ComingSoon/ComingSoon.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import style from "./ComingSoon.module.scss";
-import Row from "../layouts/layout";
+import { Row } from "../layouts/layout";
 
 const ComingSoon = () => {
   return (
